f90c5a363b8e4dd2e28b5009b394cb9f3a412029
Benadette Mokandu <benadettemokandu@gmail.com>
2025-09-24 15:11:02 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -4,5 +4,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def test_client():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<parameter>')
+def test_print(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/math/<int:num1>/<op>/<int:num2>')
+def test_math(num1, op, num2):
+    if op == '+':
+        result = num1 + num2
+    elif op == '-':
+        result = num1 - num2
+    elif op == '*':
+        result = num1 * num2
+    elif op == 'div':  
+        result = num1 / num2
+    elif op == '%':    
+        result = num1 % num2
+    else:
+        return 'Invalid operation', 400
+
+    return str(result)
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
